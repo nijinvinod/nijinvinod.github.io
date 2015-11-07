7870154fef8679b4a59907bf75f307764fc66582
Vinodan <vinodan.nijin@gmail.com>
2015-11-07 14:59:20 +0530

    Firefox Fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
+	console.log("ready");
 	$("body").keydown(function(e) {
         if(e.keyCode == 27) { //escape
             utils.closePopUp();
         }
     });
+
+    utils.moveLinksUp();
 });
 
 var utils = {
 	closePopUp: function(){
 		$(".overlay").hide();
+	},
+
+	moveLinksUp: function(){
+		var links = $(".links a");
+		var i = 0;
+		// setInterval(function(){
+		// 	$(links[i]).addClass("moveUp");
+		// 	console.log(i);
+		// 	i++;
+		// },500);
 	}
 };

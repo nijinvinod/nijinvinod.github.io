52ee21c5517986f969294ca3fc55f89f3c844703
Vinodan <vinodan.nijin@gmail.com>
2015-11-07 22:16:56 +0530

    Added image with width in mobile

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,6 +1,12 @@
-NIJIN_PIC = "";
 var homeModule = angular.module('homeModule',[])
 .controller('homeController',function($scope, $window){
 	$scope.dimensions = {};
+	$scope.svg = {};
+
 	$scope.dimensions.pageHeight = $(window).height();
+
+	$scope.svg.scaling = 0.10000;
+	if($(window).width() < 780){
+		$scope.svg.scaling = 0.075000;
+	}
 });

da7e4e75fb925ba3551fcd04345ca06923345260
Vinodan <vinodan.nijin@gmail.com>
2015-11-07 16:58:29 +0530

    Added Mobile Code

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -2,5 +2,5 @@ NIJIN_PIC = "";
 var homeModule = angular.module('homeModule',[])
 .controller('homeController',function($scope, $window){
 	$scope.dimensions = {};
-	 $scope.dimensions.pageHeight = $(window).height();
+	$scope.dimensions.pageHeight = $(window).height();
 });
